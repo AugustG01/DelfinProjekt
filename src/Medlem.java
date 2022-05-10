8ef7926f1ab70c331c91c8a9f8eb8c13bd5dc890
AugustG01 <augu8204@stud.kea.dk>
2022-05-10 11:48:17 +0200

    Medlem, database og filhåndtering

---
+++ src/Medlem.java
@@ -0,0 +1,7 @@
+public class Medlem {
+    private int alder;
+    private String navn;
+    private boolean aktivtMedlemskab;
+    private boolean konkurrenceSvømmer;
+    private boolean restance;
+}
