dbab69d5aae531dbb239205cf103ec5072e2a032
AugustG01 <augu8204@stud.kea.dk>
2022-05-10 12:57:04 +0200

    Opdateret kode, nu kan vi indlæse data fra fil

--- src/CSVhåndtering.java
+++ src/CSVhåndtering.java
@@ -1,4 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class CSVhåndtering {
+    String filnavn;
+    private Scanner filScanner;
+    PrintStream printStream;
+
+
+    public CSVhåndtering(String filnavn) throws FileNotFoundException {
+        this.filnavn = filnavn;
+    }
+
+    public void indlæsMedlemmer() throws FileNotFoundException {
+        filScanner = new Scanner(new File(filnavn)).useDelimiter(";");
+        int alder = filScanner.nextInt();
+        String navn = filScanner.next();
+        boolean aktivtMedlemskab = filScanner.nextBoolean();
+        boolean konkurrenceSvømmer = filScanner.nextBoolean();
+        boolean restance = filScanner.nextBoolean();
+        Medlem indlæsMedlem = new Medlem(alder, navn, aktivtMedlemskab, konkurrenceSvømmer, restance);
+
+    }
+
+    public void skrivMedlem(Medlem medlem) throws FileNotFoundException {
+        printStream = new PrintStream(filnavn);
+
+        printStream.print(medlem.getAlder());
+        printStream.print(";");
+        printStream.print(medlem.getNavn());
+        printStream.print(";");
+        printStream.print(medlem.getAktivtMedlemskab());
+        printStream.print(";");
+        printStream.print(medlem.getKonkurrenceSvømmer());
+        printStream.print(";");
+        printStream.print(medlem.getRestance());
+    }
 
 
 
--- src/DataBase.java
+++ src/DataBase.java
@@ -1,2 +1,21 @@
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
 public class DataBase {
+    ArrayList<Medlem> medlemmer = new ArrayList<>();
+    String filnavn = "medlemmer.csv";
+
+    // TODO: 10-05-2022 håndter throw
+    public void skriv(Medlem medlem) throws FileNotFoundException {
+        filnavn = "medlemmer.csv";
+
+        CSVhåndtering csv = new CSVhåndtering(filnavn);
+        csv.skrivMedlem(medlem);
+    }
+
+    public void indlæs() throws FileNotFoundException {
+        CSVhåndtering csv = new CSVhåndtering(filnavn);
+        csv.indlæsMedlemmer();
+    }
 }
