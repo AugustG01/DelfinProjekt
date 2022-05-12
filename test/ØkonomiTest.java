6f894a9519b806c63d7e4114d3e19c04b6598326
AugustG01 <augu8204@stud.kea.dk>
2022-05-12 14:56:39 +0200

    junit

--- test/ØkonomiTest.java
+++ test/ØkonomiTest.java
@@ -1,12 +1,16 @@
+import java.io.FileNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ØkonomiTest {
 
     @org.junit.jupiter.api.Test
-    void udregning() {
-      //Arrange
-        udregning();
+    void udregning() throws FileNotFoundException {
+        Controller controller = new Controller();
+        //DataBase db = new DataBase();
+        controller.indlæsMedlemmer();
+        Økonomi økonomi = new Økonomi(controller.db.medlemmer);
 
-       // assertEquals(2200);
+        assertEquals(3100, økonomi.udregning());
     }
 }
